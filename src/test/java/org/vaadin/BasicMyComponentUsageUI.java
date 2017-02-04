53a0afa8455c117497e053156cc0fac4036b14fd
adam <adam@vaadin.com>
2017-02-04 20:28:39 +0100

    Update test file to use Java Beans as suggestions

--- src/test/java/org/vaadin/BasicMyComponentUsageUI.java
+++ src/test/java/org/vaadin/BasicMyComponentUsageUI.java
@@ -1,10 +1,8 @@
 package org.vaadin;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Layout;
@@ -22,25 +20,79 @@ public class BasicMyComponentUsageUI extends AbstractTest {
 
         TextField textField = new TextField();
 
-        AutocompleteExtension autocompleteExtension = new AutocompleteExtension();
+        AutocompleteExtension<Planet> autocompleteExtension = new AutocompleteExtension<>();
         autocompleteExtension.extend(textField);
 
-        autocompleteExtension.setSuggestionGenerator(this::suggest);
+        autocompleteExtension.setSuggestionGenerator(this::suggest, null,
+                (planet, query) -> {
+                    StringBuffer r = new StringBuffer();
+
+                    query = query.toLowerCase();
+
+                    String n = planet.getName().toLowerCase();
+                    int iStart = n.indexOf(query);
+                    int iEnd = iStart + query.length();
+
+                    if (iStart >= 0) {
+                        r.append(planet.getName().substring(0, iStart));
+                        r.append("<b>");
+                        r.append(planet.getName().substring(iStart, iEnd));
+                        r.append("</b>");
+                        r.append(planet.getName().substring(iEnd, n.length()));
+                    } else {
+                        r.append(planet.getName());
+                    }
+
+                    return r.toString();
+                });
 
         layout.addComponent(textField);
 
         return layout;
     }
 
-    private final static List<String> PLANETS = Arrays
-            .asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
-                    "Uranus", "Neptune");
+    private final static List<Planet> PLANETS = new ArrayList<>();
 
-    private List<String> suggest(String query, int count) {
+    static {
+        int i = 0;
+        PLANETS.add(new Planet(++i, "Mercury"));
+        PLANETS.add(new Planet(++i, "Venus"));
+        PLANETS.add(new Planet(++i, "Earth"));
+        PLANETS.add(new Planet(++i, "Mars"));
+        PLANETS.add(new Planet(++i, "Jupiter"));
+        PLANETS.add(new Planet(++i, "Saturn"));
+        PLANETS.add(new Planet(++i, "Uranus"));
+        PLANETS.add(new Planet(++i, "Neptune"));
+    }
+
+    private List<Planet> suggest(String query, int count) {
         String queryLow = query.toLowerCase();
         return PLANETS.stream()
-                .filter(s -> s.toLowerCase().startsWith(queryLow)).limit(count)
-                .collect(Collectors.toList());
+                .filter(p -> p.getName().toLowerCase().contains(queryLow))
+                .limit(count).collect(Collectors.toList());
+    }
+
+    private static class Planet {
+        private int position;
+        private String name;
+
+        public Planet(int position, String name) {
+            this.position = position;
+            this.name = name;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return getName();
+        }
     }
 
 }
