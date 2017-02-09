c18603c7dcdc765eaa660b179c8e52af0083d73b
adam <adam@vaadin.com>
2017-02-09 12:43:52 +0100

    Styling of test UI

--- src/test/java/org/vaadin/addons/UsersComponentUsageUI.java
+++ src/test/java/org/vaadin/addons/UsersComponentUsageUI.java
@@ -3,11 +3,13 @@ package org.vaadin.addons;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.text.WordUtils;
 import org.vaadin.addonhelpers.AbstractTest;
 
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
+import com.vaadin.server.Page;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.TextField;
@@ -21,13 +23,16 @@ public class UsersComponentUsageUI extends AbstractTest {
         Layout layout = new VerticalLayout();
 
         TextField textField = new TextField();
+        textField.setWidth(250, Unit.PIXELS);
 
-        AutocompleteExtension<DataSource.User> autocompleteExtension = new AutocompleteExtension<>(
-                textField);
+        AutocompleteExtension<DataSource.User> autocompleteExtension =
+                new AutocompleteExtension<>(textField);
 
         autocompleteExtension.setSuggestionGenerator(this::getSuggestions,
                 DataSource.User::getName, this::convertCaption);
         layout.addComponent(textField);
+
+        setStyles();
 
         return layout;
     }
@@ -44,15 +49,26 @@ public class UsersComponentUsageUI extends AbstractTest {
     private SafeHtml convertCaption(DataSource.User user, String query) {
         SafeHtmlBuilder builder = new SafeHtmlBuilder();
 
-        builder.appendHtmlConstant(
-                "<div style='display:flex; align-items:center; padding-top:2px; padding-bottom:2px;'>");
-        builder.append(SafeHtmlUtils
-                .fromTrustedString("<img src='" + user.getPicture() + "'>"));
-        builder.appendHtmlConstant("<span style='padding-left: 10px'>");
-        builder.appendEscaped(user.getName());
+        builder.appendHtmlConstant("<div class='suggestion-container'>");
+        builder.append(SafeHtmlUtils.fromTrustedString(
+                "<img src='" + user.getPicture() + "' class='userimage'>"));
+        builder.appendHtmlConstant("<span class='username'>");
+        builder.appendEscaped(WordUtils.capitalizeFully(user.getName(), ' '));
         builder.appendHtmlConstant("</span>");
         builder.appendHtmlConstant("</div>");
 
         return builder.toSafeHtml();
+    }
+
+    private void setStyles() {
+        Page.Styles styles = Page.getCurrent().getStyles();
+        styles.add(".suggestion-container{"
+                + "display:flex;"
+                + "align-items:center;"
+                + "padding-top:2px;"
+                + "padding-bottom:2px;"
+                + "}");
+        styles.add(".userimage{border-radius: 50%;}");
+        styles.add(".username{padding-left: 10px;}");
     }
 }
