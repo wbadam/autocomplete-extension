fc608d87306bd6838c196c032b29e8b2362ce5ba
adam <adam@vaadin.com>
2017-03-27 20:58:34 +0300

    Fix widgetset path for test files

--- src/test/java/org/vaadin/addonhelpers/Config.java
+++ src/test/java/org/vaadin/addonhelpers/Config.java
@@ -5,7 +5,7 @@ import com.vaadin.annotations.Widgetset;
 /**
  * Configuration file for UI test cases
  */
-@Widgetset("org.vaadin.addons.Widgetset")
+@Widgetset("org.vaadin.addons.autocomplete.Widgetset")
 public class Config {
 
 }
